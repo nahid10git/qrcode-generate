a7b0bd89ffc490d671d8f7247f21c32792e28242
nahid10git <nahidrashel@gmail.com>
2023-04-24 22:05:43 +0600

    QR Code Generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { QRCode } from 'qrcode';
 import './App.css';
 
 function App() {
+
+const [qrCodeData, setQrCodeData]= useState('');
+
+const [qrCodeImage, setQRCodeImage]=useState('');
+
+const generateQRCode = () =>{
+  QRCode.toDataURL(qrCodeData, (err, url)=>{
+    if(err) return console.error("Failed to generate QR Code", err);
+    setQRCodeImage(url);
+  })
+
+}
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>QR Code Generator</h2>
+      <div>
+        <input type='text' value={qrCodeData}
+        onChange={e=>setQrCodeData(e.target.value)}
+        
+        placeholder="Enter data to qrcode"/>
+        <button onClick={generateQRCode}>Generate</button>
+        
+        
+      </div>
+      
     </div>
   );
 }
